f90b101baf771a56c2f190d0a1f5375a13ff8ef9
Jason Bishop <j.bishop29@hotmail.com>
2019-02-28 14:28:53 +0000

    Fix  allowed host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), "https://jaysson29-ecomerce.herokuapp.com"]
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), "jaysson29-ecomerce.herokuapp.com"]
 
 
 # Application definition
